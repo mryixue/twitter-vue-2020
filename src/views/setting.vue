05dcd07920997d272365037a2e194230b77e820d
TC Wang <tingchun0113@gmail.com>
2021-04-25 23:16:32 +0800

    Add apis

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="mainframe">
     <mainLeft/>
-<<<<<<< HEAD
-    <settingDetail />
-    <mainRightFollow/>
-=======
     <setting/>
->>>>>>> e06b1287e03fc0a590b5904a54da5beff4074b88
   </div>
 </template>
 
 <script>
 import mainLeft from '../components/mainLeft'
-<<<<<<< HEAD
-import mainRightFollow from '../components/mainRightFollow'
-import settingDetail from '../components/settingDetail'
-
-export default {
-  components: {
-    mainLeft,
-    mainRightFollow,
-    settingDetail
-  },
-
-=======
 import setting from '../components/setting'
 export default {
   components: {
     mainLeft,
     setting
   },
->>>>>>> e06b1287e03fc0a590b5904a54da5beff4074b88
 }
 </script>
 
@@ -40,20 +22,6 @@ export default {
   height: 100vh
   display: grid
   grid-template:
-<<<<<<< HEAD
-    columns: 20vw 1fr 25vw
-    rows: 20vh 1fr
-    areas: "left new right" "left article right"
-  position: relative
-  #mainLeft
-    grid-area: left
-  #settingDetail
-    grid-area: article
-  #mainRightFollow
-    grid-area: right
-</style>
-
-=======
     columns: 20vw 1fr
     areas: "left setting"
   position: relative
@@ -62,4 +30,3 @@ export default {
   #setting
     grid-area: setting
 </style>
->>>>>>> e06b1287e03fc0a590b5904a54da5beff4074b88
